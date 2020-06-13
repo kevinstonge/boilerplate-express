401f6b90efd81029c5e934bdbe51cdd89faa9d1c
Kevin St.Onge <kevinstonge@gmail.com>
2020-06-13 15:29:55 -0400

    route params

--- myApp.js
+++ myApp.js
@@ -45,7 +45,7 @@ app.get("/json",(req,res)=>{
 app.get("/now",(req,res,next)=>{req.time = new Date().toString(); next();},(req,res)=>{res.json({"time":req.time})})
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo",(req,res)=>{res.json({"echo":req.params.word})})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
